a45db527ad27c2fc673f8426a85b125bc526068c
AndresFForeroP <andres2200500@correo.uis.edu.co>
2025-05-02 17:59:53 -0500

    avance

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
 "use strict";
-import {listaequipos_objeto} from "/data/equipos.js";
-alert(listaequipos_objeto);
+import {Nuevo_equipo,mostrar_equipos} from "/data/equipos.js";
+const div_listaequipos = document.querySelector('#equipos');
+const btn_vaciar = document.querySelector("#btn-vaciar")
+let listaEquipos = [];
+console.log(listaEquipos);
+listaEquipos.push(Nuevo_equipo("colombia"))
+listaEquipos.push(Nuevo_equipo("ecuador"))
+listaEquipos.push(Nuevo_equipo("venezuela"))
+div_listaequipos.innerHTML = mostrar_equipos(listaEquipos)
+console.log(mostrar_equipos(listaEquipos))
+
+btn_vaciar.addEventListener("click",(e) =>{
+    Swal.fire({
+        title: "Desea eliminar todos los equipos?",
+        showDenyButton: true,
+        confirmButtonText: "Confirmar",
+        denyButtonText: `Cancelar`
+      }).then((result) => {
+        if (result.isConfirmed) {
+            btn_vaciar.disabled = true;
+            listaEquipos = []
+            div_listaequipos.innerHTML = mostrar_equipos(listaEquipos)
+          Swal.fire("Eliminados", "", "success");
+        } else if (result.isDenied) {
+          Swal.fire("No se ha eliminado ningun equipo", "", "error");
+        }
+      });
+      e.preventDefault();
+})
--- data/equipos.js
+++ data/equipos.js
@@ -1,13 +1,36 @@
-export const listaequipos_objeto = {
-    "bucaramanga":{
+export const Nuevo_equipo = (nEquipo) =>{
+    let boton_vaciar = document.querySelector("#btn-vaciar")
+    let plantilla = {
+        "nombre":nEquipo,
         "DT":"",
-        "CM":{},
-        "jugadores":{
-            "messi":{
-                "dorsal":10,
-                "posicion":"Delantero",
-                "edad":30
-            }
-        }
+        "CM":[],
+        "jugadores":[]
     }
+    boton_vaciar.disabled = false
+    return plantilla
+}
+export const mostrar_equipos = (listaEquipos) =>{
+    let datos_equipos = ""
+    let datos_equipo = ""
+    if (listaEquipos.length == 0) {
+        datos_equipos = `<p class="sin-equipos">NO HAY EQUIPOS REGISTRADOS</p>`
+    }
+    else{
+        listaEquipos.forEach(equipo => {
+            datos_equipo = `<div class="equipos">
+                <p><strong>NOMBRE EQUIPO</strong> <br>
+                ${equipo.nombre}</p>
+                <p><strong>DIRECTOR TECNICO</strong> <br>
+                "${equipo.DT}"</p>
+                <p><strong>CUERPO MEDICO</strong> <br>
+                <span>${equipo.CM.length}</span></p>
+                <p><strong>JUGADORES</strong> <br><br>
+                <span>${equipo.jugadores.length}</span></p>
+                <button class="boton-editar"><span class="material-symbols-outlined">edit</span></button>
+                <button class="boton-eliminar"><span class="material-symbols-outlined">delete</span></button>
+            </div>`
+            datos_equipos += datos_equipo
+        });
+    }
+    return datos_equipos
 }
