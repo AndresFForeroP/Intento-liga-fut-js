b4c25fb3e1811e9d2e42eff17ed2d5d7c4beb487
AndresFForeroP <andres2200500@correo.uis.edu.co>
2025-05-03 13:30:10 -0500

    Boton eliminar equipo no manda señal,componentes

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 "use strict";
-import {Nuevo_equipo,mostrar_equipos} from "/data/equipos.js";
+import {Nuevo_equipo,mostrar_equipos} from "./data/equipos.js";
 const div_listaequipos = document.querySelector('#equipos');
 const btn_vaciar = document.querySelector("#btn-vaciar")
+const btn_nuevo = document.querySelector("#btn-nuevo")
 let listaEquipos = [];
-console.log(listaEquipos);
-listaEquipos.push(Nuevo_equipo("colombia"))
-listaEquipos.push(Nuevo_equipo("ecuador"))
-listaEquipos.push(Nuevo_equipo("venezuela"))
+listaEquipos.push(Nuevo_equipo("argentina","messi"))
 div_listaequipos.innerHTML = mostrar_equipos(listaEquipos)
 console.log(mostrar_equipos(listaEquipos))
-
 btn_vaciar.addEventListener("click",(e) =>{
     Swal.fire({
         title: "Desea eliminar todos los equipos?",
@@ -27,4 +24,36 @@ btn_vaciar.addEventListener("click",(e) =>{
         }
       });
       e.preventDefault();
+})
+btn_nuevo.addEventListener("click",(e) =>{
+  let nombre = "";
+  let DT = "";
+  Swal.fire({
+    title: "Ingrese el nombre del nuevo equipo",
+    input: "text",
+    inputAttributes: {
+      autocapitalize: "off"
+    },
+    showCancelButton: true,
+    confirmButtonText: "Crear",
+    showLoaderOnConfirm: true,
+    preConfirm: async (text) => {
+      nombre = text;
+      Swal.fire({
+        title: `Ingrese el nombre del Director tecnico de ${nombre}`,
+        input: "text",
+        inputAttributes: {
+        autocapitalize: "off"
+        },
+        showCancelButton: true,
+        confirmButtonText: "Crear",
+        showLoaderOnConfirm: true,
+        preConfirm: async (texto) => {
+          DT = texto;
+          listaEquipos.push(Nuevo_equipo(nombre,DT))
+          div_listaequipos.innerHTML = mostrar_equipos(listaEquipos)
+          console.log(listaEquipos);
+        }})
+    }})
+  
 })
--- data/equipos.js
+++ data/equipos.js
@@ -1,14 +1,19 @@
-export const Nuevo_equipo = (nEquipo) =>{
+export const Nuevo_equipo = (nEquipo,dt) =>{
     let boton_vaciar = document.querySelector("#btn-vaciar")
     let plantilla = {
         "nombre":nEquipo,
-        "DT":"",
+        "DT":dt,
         "CM":[],
         "jugadores":[]
     }
     boton_vaciar.disabled = false
     return plantilla
 }
+const btn_eliminar = (equipo) => {
+    console.log(equipo)
+  }
+btn_eliminar("1")
+
 export const mostrar_equipos = (listaEquipos) =>{
     let datos_equipos = ""
     let datos_equipo = ""
@@ -21,13 +26,13 @@ export const mostrar_equipos = (listaEquipos) =>{
                 <p><strong>NOMBRE EQUIPO</strong> <br>
                 ${equipo.nombre}</p>
                 <p><strong>DIRECTOR TECNICO</strong> <br>
-                "${equipo.DT}"</p>
+                ${equipo.DT}</p>
                 <p><strong>CUERPO MEDICO</strong> <br>
                 <span>${equipo.CM.length}</span></p>
                 <p><strong>JUGADORES</strong> <br><br>
                 <span>${equipo.jugadores.length}</span></p>
                 <button class="boton-editar"><span class="material-symbols-outlined">edit</span></button>
-                <button class="boton-eliminar"><span class="material-symbols-outlined">delete</span></button>
+                <button onclick="${btn_eliminar(equipo.nombre)}" class="boton-eliminar"><span class="material-symbols-outlined">delete</span></button>
             </div>`
             datos_equipos += datos_equipo
         });
