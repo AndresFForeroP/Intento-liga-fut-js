6282f326a7a27c8d55f9a09746bfc7a359061174
AndresFForeroP <andres2200500@correo.uis.edu.co>
2025-05-06 16:28:52 -0500

    p.c.s arregado,botones con iconos no aparecen

--- data/equipos.js
+++ data/equipos.js
@@ -12,43 +12,53 @@ export const Nuevo_equipo = (nEquipo,dt) =>{
 export const mostrar_equipos = (listaEquipos) => {
     class Micomponente extends HTMLElement {
         constructor() {
+            const estilo = document.createElement('style');
+            estilo.innerHTML = /*css*/`
+                    .material-symbols-outlined {
+                        margin-top: 0.2rem;
+                        font-variation-settings:
+                        'FILL' 0,
+                        'wght' 400,
+                        'GRAD' 0,
+                        'opsz' 24
+                    }
+                    div{margin-top: 1rem;
+                    margin-left: 1rem;
+                    border-radius: 1.5rem;
+                    display: grid;
+                    grid-template-columns: repeat(6,16.67%);
+                    background:
+                        rgba(66, 66, 66, 0.7);
+                    p{
+                        font-size: 1rem;
+                        text-align: center;
+                        margin: 1rem;
+                        margin-left: 1rem;
+                        span{
+                            font-size: 1.3rem;
+                        }
+                    }
+                    button{
+                        margin: auto;
+                    }
+                    .boton-eliminar{
+                        background-color: var(--segundocolor);
+                    }
+                    .boton-editar{
+                        margin-right: -1.7rem;
+                        background-color: var(--tercercolor);
+                    }
+                    strong{
+                        color:var(--primercolor);
+                    }}`;
             super();
             this.attachShadow({ mode: "open" });
+            this.shadowRoot.appendChild(estilo);
             listaEquipos.forEach(equipo => {
+                    
                 const equipoDiv = document.createElement('div');
                 equipoDiv.classList.add('equipos');
                 equipoDiv.innerHTML = /*html*/`
-                    <style>
-                        margin-top: 1rem;
-                        margin-left: 1rem;
-                        border-radius: 1.5rem;
-                        display: grid;
-                        grid-template-columns: repeat(6,16.67%);
-                        background:
-                            rgba(66, 66, 66, 0.7);
-                        p{
-                            font-size: 1rem;
-                            text-align: center;
-                            margin: 1rem;
-                            margin-left: 1rem;
-                            span{
-                                font-size: 1.3rem;
-                            }
-                        }
-                        button{
-                            margin: auto;
-                        }
-                        .boton-eliminar{
-                            background-color: var(--segundocolor);
-                        }
-                        .boton-editar{
-                            margin-right: -1.7rem;
-                            background-color: var(--tercercolor);
-                        }
-                        strong{
-                            color:var(--primercolor);
-                        }
-                    </style>
                     <p><strong>NOMBRE EQUIPO</strong> <br>
                     ${equipo.nombre}</p>
                     <p><strong>DIRECTOR TECNICO</strong> <br>
